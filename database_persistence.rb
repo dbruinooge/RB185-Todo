3a15928c7cb9d799d1f6b162b841c3a1b1b71ded
Derek Bruinooge <derek.bruinooge@gmail.com>
2022-04-27 07:27:35 +0000

    Update for Heroku deployment

--- database_persistence.rb
+++ database_persistence.rb
@@ -1,15 +1,23 @@
 require "pg"
-require "pry"
 
 class DatabasePersistence
   def initialize(logger)
     @db = PG.connect(dbname: "todos")
+    @db = if Sinatra::Base.production?
+            PG.connect(ENV['DATABASE_URL'])
+          else
+            PG.connect(dbname: "todos")
+          end
     @logger = logger
   end
 
   def query(statement, *params)
     @logger.info "#{statement}: #{params}"
     @db.exec_params(statement, params)
+  end
+
+  def disconnect
+    @db.close
   end
 
   def find_list(id)
